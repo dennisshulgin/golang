158564c6ba2ffb4e1b478379e5bd6309a4c638e7
Denis <denisshulgin2017@yandex.ru>
2026-07-22 20:28:56 +0300

    style

---
+++ errors/config.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"errors"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+var Properties = []string{"host", "debug", "port"}
+var BooleanValues = []string{"false", "true"}
+
+var ErrInvalidPropertyLine = errors.New("Invalid property line")
+var ErrInvalidHost = errors.New("Invalid host")
+var ErrInvalidPort = errors.New("Invalid port")
+var ErrinvalidDebug = errors.New("Invalid debug")
+var ErrUnknownProperty = errors.New("Unknown property")
+
+type Config struct {
+	Host  string
+	Port  int
+	Debug bool
+}
+
+func ParseConfig(input string) (Config, error) {
+	config := Config{}
+
+	lines := strings.Split(input, "\n")
+
+	for _, line := range lines {
+		property := strings.Split(line, "=")
+		if len(property) != 2 {
+			return config, ErrInvalidPropertyLine
+		}
+
+		propertyError := validateProperty(property[0], property[1])
+
+		if propertyError != nil {
+			return config, propertyError
+		}
+
+		setProperty(&config, property[0], property[1])
+	}
+
+	return config, nil
+}
+
+func validateProperty(propertyName string, propertyValue string) error {
+	if !slices.Contains(Properties, propertyName) {
+		return ErrUnknownProperty
+	}
+
+	var propertyError error = nil
+
+	if propertyName == "host" {
+		propertyError = validateHost(propertyValue)
+	}
+
+	if propertyName == "debug" {
+		propertyError = validateDebug(propertyValue)
+	}
+
+	if propertyName == "port" {
+		propertyError = validatePort(propertyValue)
+	}
+	return propertyError
+}
+
+func validatePort(portAsString string) error {
+	if len(portAsString) == 0 {
+		return ErrInvalidPort
+	}
+
+	for i := 0; i < len(portAsString); i++ {
+		if portAsString[i] < '0' || portAsString[i] > '9' {
+			return ErrInvalidPort
+		}
+	}
+
+	port, err := strconv.Atoi(portAsString)
+	if err != nil || port < 1 || port > 65535 {
+		return ErrInvalidPort
+	}
+
+	return nil
+}
+
+func validateDebug(debugAsString string) error {
+	if !slices.Contains(BooleanValues, debugAsString) {
+		return ErrinvalidDebug
+	}
+
+	return nil
+}
+
+func validateHost(hostAsString string) error {
+	trimmedHost := strings.TrimSpace(hostAsString)
+	if trimmedHost == "" {
+		return ErrInvalidHost
+	}
+
+	return nil
+}
+
+func setProperty(config *Config, propertyName string, propertyValue string) {
+	if propertyName == "host" {
+		config.Host = propertyValue
+	}
+	if propertyName == "port" {
+		port, _ := strconv.Atoi(propertyValue)
+		config.Port = port
+	}
+	if propertyName == "debug" {
+		debug, _ := strconv.ParseBool(propertyValue)
+		config.Debug = debug
+	}
+}
--- errors/errors.go
+++ errors/errors.go
@@ -1,28 +1,9 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"os"
-	"slices"
-	"strconv"
-	"strings"
 )
-
-type Config struct {
-	Host  string
-	Port  int
-	Debug bool
-}
-
-var Properties = []string{"host", "debug", "port"}
-var booleanValues = []string{"false", "true"}
-
-var ErrInvalidPropertyLine = errors.New("Invalid property line")
-var ErrInvalidHost = errors.New("Invalid host")
-var ErrInvalidPort = errors.New("Invalid port")
-var ErrinvalidDebug = errors.New("Invalid debug")
-var ErrUnknownProperty = errors.New("Unknown property")
 
 func main() {
 	input, errReadFile := os.ReadFile("config.txt")
@@ -39,99 +20,5 @@ func main() {
 	} else {
 		fmt.Println("Config is valid")
 		fmt.Println(config)
-	}
-}
-
-func ParseConfig(input string) (Config, error) {
-	config := Config{}
-
-	lines := strings.Split(input, "\n")
-
-	for _, line := range lines {
-		property := strings.Split(line, "=")
-		if len(property) != 2 {
-			return config, ErrInvalidPropertyLine
-		}
-
-		propertyError := validateProperty(property[0], property[1])
-
-		if propertyError != nil {
-			return config, propertyError
-		}
-
-		setProperty(&config, property[0], property[1])
-	}
-
-	return config, nil
-}
-
-func validateProperty(propertyName string, propertyValue string) error {
-	if !slices.Contains(Properties, propertyName) {
-		return ErrUnknownProperty
-	}
-
-	var propertyError error = nil
-
-	if propertyName == "host" {
-		propertyError = validateHost(propertyValue)
-	}
-
-	if propertyName == "debug" {
-		propertyError = validateDebug(propertyValue)
-	}
-
-	if propertyName == "port" {
-		propertyError = validatePort(propertyValue)
-	}
-	return propertyError
-}
-
-func validatePort(portAsString string) error {
-	if len(portAsString) == 0 {
-		return ErrInvalidPort
-	}
-
-	for i := 0; i < len(portAsString); i++ {
-		if portAsString[i] < '0' || portAsString[i] > '9' {
-			return ErrInvalidPort
-		}
-	}
-
-	port, err := strconv.Atoi(portAsString)
-	if err != nil || port < 1 || port > 65535 {
-		return ErrInvalidPort
-	}
-
-	return nil
-}
-
-func validateDebug(debugAsString string) error {
-	if !slices.Contains(booleanValues, debugAsString) {
-		return ErrinvalidDebug
-	}
-
-	return nil
-}
-
-func validateHost(hostAsString string) error {
-	trimmedHost := strings.TrimSpace(hostAsString)
-	if trimmedHost == "" {
-		return ErrInvalidHost
-	}
-
-	return nil
-}
-
-func setProperty(config *Config, propertyName string, propertyValue string) {
-	if propertyName == "host" {
-		config.Host = propertyValue
-	}
-	if propertyName == "port" {
-		port, _ := strconv.Atoi(propertyValue)
-		config.Port = port
-	}
-	if propertyName == "debug" {
-		debug, _ := strconv.ParseBool(propertyValue)
-		config.Debug = debug
 	}
 }
